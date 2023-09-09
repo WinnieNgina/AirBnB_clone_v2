1dde76f1bbaec346f95c9e26543f898f15c7f83d
Winfred <winfrednginakilonzo@gmail.com>
2023-09-09 14:57:04 +0000

    Deploy both locally and on the server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,17 @@
-#!/usr/bin/python3
-# Fabfile that distributes an archive to your web servers
+t distributes an archive to your web servers
 from fabric.api import *
 from datetime import datetime
 import os
 
-env.hosts = ['52.91.168.104', '100.24.253.204']
+env.hosts = ['localhost', '52.91.168.104', '100.24.253.204']
 env.user = 'ubuntu'
 # env.key_filename = '~/.ssh/id_rsa'
 
+def is_local():
+    """
+    Check if the script is running locally by checking an environment variable.
+    """
+    return os.getenv('IS_LOCAL') == 'true'
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
@@ -20,22 +24,35 @@ def do_pack():
     else:
         return archive_path
 
-
 def do_deploy(archive_path):
-    '''use os module to check for valid file path'''
+    '''Use os module to check for valid file path'''
     if os.path.exists(archive_path):
         archive = archive_path.split('/')[1]
         a_path = "/tmp/{}".format(archive)
         folder = archive.split('.')[0]
         f_path = "/data/web_static/releases/{}/".format(folder)
 
-        put(archive_path, a_path)
-        run("mkdir -p {}".format(f_path))
-        run("tar -xzf {} -C {}".format(a_path, f_path))
-        run("rm {}".format(a_path))
-        run("mv -f {}web_static/* {}".format(f_path, f_path))
-        run("rm -rf {}web_static".format(f_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(f_path))
+        if is_local():  # Check if the script is running locally
+            local("mkdir -p {}".format(f_path))
+            local("tar -xzf {} -C {}".format(a_path, f_path))
+            local("rm {}".format(a_path))
+            local("mv -f {}web_static/* {}".format(f_path, f_path))
+            local("rm -rf {}web_static".format(f_path))
+            local("rm -rf /data/web_static/current")
+            local("ln -s {} /data/web_static/current".format(f_path))
+        else:
+            put(archive_path, a_path)
+            run("mkdir -p {}".format(f_path))
+            run("tar -xzf {} -C {}".format(a_path, f_path))
+            run("rm {}".format(a_path))
+            run("mv -f {}web_static/* {}".format(f_path, f_path))
+            run("rm -rf {}web_static".format(f_path))
+            run("rm -rf /data/web_static/current")
+            run("ln -s {} /data/web_static/current".format(f_path))
+
         return True
     return False
+
+# Example usage: fab deploy (for remote server deployment)
+# Example usage: IS_LOCAL=true fab deploy (for local deployment)
+
