1aacb78867366c0ebf9d4a9459694065775537be
Winfred <winfrednginakilonzo@gmail.com>
2023-09-22 11:11:11 +0300

    fix pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """display Hello HBNB!"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     '''
     script that starts a Flask web application:
@@ -14,4 +15,4 @@ def hello_hbnb():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
